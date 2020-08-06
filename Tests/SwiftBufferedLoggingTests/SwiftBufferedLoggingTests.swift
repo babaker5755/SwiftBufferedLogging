c04fe6934936df09db96543c1a9c484dd4b340a5
babaker5755 <bbrandonbaker.b@gmail.com>
2020-08-06 12:37:25 -0400

    Passes basic tests

--- Tests/SwiftBufferedLoggingTests/SwiftBufferedLoggingTests.swift
+++ Tests/SwiftBufferedLoggingTests/SwiftBufferedLoggingTests.swift
@@ -10,34 +10,33 @@ final class SwiftBufferedLoggingTests: XCTestCase {
     /// Tests timer by logging once
     /// and checking if it has been sent
     /// after logOption.saveTime
-    func simpleTimerTest() {
+    func testSimpleTimer() {
         log("test")
-        performIn(6) {
-            XCTAssertEqual(SwiftBufferedLoggingTests.sentLogs, ["test"])
-        }
+        wait(for: 6)
+        XCTAssertEqual(SwiftBufferedLoggingTests.sentLogs, ["test"])
     }
     
     /// Test max buffer size
     /// by adding 1 more than buffer size
     /// and checking that they are sent
     /// before logOption.saveTime
-    func simpleMaxBufferTest() {
+    func testSimpleMaxBuffer() {
         
         // max buffer size is 5
-        let testArray = ["test", "test1", "test2", "test3", "test2", "test3"]
-        
+        let testArray = ["test", "test1", "test2", "test3", "test4", "test5"]
+        let resultArray = ["test", "test1", "test2", "test3", "test4"]
         testArray.forEach { log($0) }
         
-        performIn(1) {
-            XCTAssertEqual(SwiftBufferedLoggingTests.sentLogs, ["test"])
-        }
+        wait(for: 1)
+        
+        XCTAssertEqual(SwiftBufferedLoggingTests.sentLogs, resultArray)
     }
     
     /// Tests min buffer by adding 1 below
     /// min buffer size, and checking that
     /// they have not been sent after
     /// logOption.saveTime has passed
-    func simpleMinBufferTest() {
+    func testSimpleMinBuffer() {
         
         // min buffer size = 5
         let logOptions = LogOptions(saveTime: 5, maxBufferSize: 10, minBufferSize: 5, maxRetries: 0)
@@ -47,13 +46,30 @@ final class SwiftBufferedLoggingTests: XCTestCase {
         
         testArray.forEach { log($0) }
         
-        performIn(6) {
-            XCTAssertEqual(SwiftBufferedLoggingTests.sentLogs, [])
-        }
+        wait(for: 6)
+        
+        XCTAssertEqual(SwiftBufferedLoggingTests.sentLogs, [])
+        
     }
     
     
 }
+
+//  MARK: User Delegate Function
+extension SwiftBufferedLoggingTests : SwiftBufferedLogDelegate {
+    
+    func sendLogs(_ logs: [Log], completion: ((Bool) -> Void)) {
+        print("adding logs to sent logs", logs.map { $0.message })
+        SwiftBufferedLoggingTests.sentLogs.append(contentsOf: logs.map { $0.message })
+        completion(true)
+    }
+    
+    func didFailToSendLogs(_ logs: [Log]) {
+        print("Logs failed to send after retrying: \(logs.map { $0.message + "\n" })")
+    }
+    
+}
+
 
 
 // MARK: Test Helper Functions
@@ -61,27 +77,39 @@ extension SwiftBufferedLoggingTests {
     
     /// All tests
     static var allTests = [
-        ("simpleTimerTest", simpleTimerTest),
-        ("simpleMaxBufferTest", simpleMaxBufferTest),
-        ("simpleMinBufferTest", simpleMinBufferTest)
+        ("testSimpleTimer", testSimpleTimer),
+        ("testSimpleMaxBuffer", testSimpleMaxBuffer),
+        ("testSimpleMinBuffer", testSimpleMinBuffer)
     ]
     
     /// Set up
     override func setUp() {
-        print("setting for next test")
+        sleep(10)
         setupLogger()
     }
     
-    /// Timer
-    func performIn(_ seconds: Double, completion: @escaping (() -> Void)) {
-        if #available(iOS 10.0, *) {
-            _ = Timer.scheduledTimer(withTimeInterval: seconds, repeats: false) { _ in
-                completion()
-            }
-        }
+    /// Tear down
+    override func tearDown() {
+        SwiftBufferedLoggingTests.sentLogs = []
     }
+    
 }
 
+// MARK: Wait
+extension XCTestCase {
+
+  func wait(for duration: TimeInterval) {
+    let waitExpectation = expectation(description: "Waiting")
+
+    let when = DispatchTime.now() + duration
+    DispatchQueue.main.asyncAfter(deadline: when) {
+      waitExpectation.fulfill()
+    }
+
+    // We use a buffer here to avoid flakiness with Timer on CI
+    waitForExpectations(timeout: duration + 0.5)
+  }
+}
 
 
 //MARK: User Set Up Functions
@@ -108,23 +136,6 @@ extension SwiftBufferedLoggingTests {
         let log = Log(message: message, level: .debug, metadata: metadata)
         
         logger.log(log)
-    }
-    
-}
-
-
-//  MARK: User Delegate Function
-extension SwiftBufferedLoggingTests : SwiftBufferedLogDelegate {
-    
-    func sendLogs(_ logs: [Log], completion: ((Bool) -> Void)) {
-        print("adding logs to sent logs", logs.map { $0.message })
-        SwiftBufferedLoggingTests.sentLogs.append(contentsOf: logs.map { $0.message })
-        completion(true)
-    }
-    
-    func didFailToSendLogs(_ logs: [Log]) {
-        print("Logs failed to send after retrying: \(logs.map { $0.message + "\n" })")
-        
     }
     
 }
